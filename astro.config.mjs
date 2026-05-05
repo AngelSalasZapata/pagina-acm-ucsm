fb75003ff9c1bfb0193a7396e0ae082ab54a969e
AngelSalasZapata <164601086+AngelSalasZapata@users.noreply.github.com>
2026-05-05 13:36:55 -0500

    Remove base path to serve from root instead of /pagina-acm-ucsm

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://AngelSalasZapata.github.io',
-  base: '/pagina-acm-ucsm',  
 });
